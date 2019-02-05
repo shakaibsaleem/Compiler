c7bf91be238ad1216a024aadc853dd9598d891d6
shakaibsaleem <ms01036@st.habib.edu.pk>
2019-02-05 14:50:47 +0500

    ignoring comment blocks preservng line number

--- Python/try.py
+++ Python/try.py
@@ -35,18 +35,33 @@ def main():
 
 	new = ""
 	ignore = False
-	for i in range(len(stream)):
-		if stream[i:i+2] == '/*':
-			ignore = True
+	line = 1 # line counter
+	start_line = 0 # to record which line our comment block started from
 
+	for i in range(len(stream)):
+		if stream[i] == '\n':
+			line += 1 # count lines
+
+		# detect when comment block starts
+		if stream[i:i+2] == '/*':
+			ignore = True # start ignoting input stream
+			start_line = line # record line number
+
+		# detect when comment block ends
 		if stream[i-2:i] == '*/' and ignore == True:
-			ignore = False
-			# i+=1
+			ignore = False # stop ignoring input stream
+
+		# ignore input stram or not
 		if ignore == False:
 			new = new + stream[i]
+		elif stream[i] == '\n':
+			# if newline character is encountered in a comment block, then dont ignore it
+			new = new + stream[i]
+
 	if ignore == True:
-		
+		with open("Errors.txt",'w') as log:
+			log.write("End of comment block (*/) is missing for the comment started in line: %d" %start_line)
 	stream = new
 	
-	print(stream)
+	# print(stream)
 main()
