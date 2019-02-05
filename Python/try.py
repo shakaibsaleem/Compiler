082cafff27e3682df93341116d836031c783174e
shakaibsaleem <ms01036@st.habib.edu.pk>
2019-02-05 14:33:08 +0500

    Update try.py

--- Python/try.py
+++ Python/try.py
@@ -3,11 +3,50 @@ def main():
 	with open('snippet.huc') as file:
 		stream = file.read()
 	# print(stream)
-	tokens = stream.splitlines()
-	print(tokens)
+	# tokens = stream.splitlines()
+	# print(tokens)
 
 	# tokens = stream.split(" ")
 	# new = [t.split(" ") for t in tokens]
 	# print(new)
 
+	new = ""
+	for i in range(len(stream)):
+		char = stream[i]
+
+		if char != '\t':
+			new = new + char
+	stream = new
+	
+	new = ""
+	ignore = False
+	for i in range(len(stream)):
+		char = stream[i]
+
+		if char == '#':
+			ignore = True
+		if char == '\n' and ignore == True:
+			ignore = False
+		if ignore == False:
+			new = new + char
+	stream = new
+	
+	# print(stream)
+
+	new = ""
+	ignore = False
+	for i in range(len(stream)):
+		if stream[i:i+2] == '/*':
+			ignore = True
+
+		if stream[i-2:i] == '*/' and ignore == True:
+			ignore = False
+			# i+=1
+		if ignore == False:
+			new = new + stream[i]
+	if ignore == True:
+		
+	stream = new
+	
+	print(stream)
 main()
